7455fbda91206cb26d7f6f64fa04109ed9fe1d0e
Erin  Rose <errose10@gmail.com>
2019-12-03 02:30:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,10 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |age|
+    if age =< 12 
+      then puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else return
+      
